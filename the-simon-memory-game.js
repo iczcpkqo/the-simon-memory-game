194ad80fb78ea2008c3d001be0c880772865d06d
Xiang Mao <xiang.mao.2021@mumail.ie>
2021-03-03 04:01:16 +0000

    using css draw game interface

---
+++ the-simon-memory-game.js
@@ -0,0 +1,8 @@
+// function about setting css, style
+
+settingContainer();
+function settingContainer() {
+    document.getElementsByClassName("container")[0].style.height = window.innerHeight + "px";
+}
+
+// function about game
