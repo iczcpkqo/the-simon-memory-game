96aeaf7a6f1cc86ac25ef9c4a019437a988aa395
Xiang Mao <xiang.mao.2021@mumail.ie>
2021-03-04 17:45:28 +0000

    fix the display of score

--- the-simon-memory-game.js
+++ the-simon-memory-game.js
@@ -1,4 +1,6 @@
-// Setting Of Constant
+/**
+ * Setting Of Constant
+ */
 const __PLATE_LIGHTEN_DURATION__ = {
     'on': 500,
     'on-250': 200,
@@ -15,7 +17,9 @@ const __STATUS_TEXT__ = {
     playing: 'PLAYING...',
 }
 
-// Setting of Global Variable
+/**
+ * Setting of Global Variable
+ */
 var g_IsGameStart;
 var g_IsDisplayOver;
 var g_IsUnlockLighten;
@@ -28,15 +32,17 @@ var g_Score_Top;
 var g_Score_Current;
 var g_Model_Current;
 
-// function about setting css, style
-
+/**
+ * function about setting css, style
+ */
 settingContainer();
 function settingContainer() {
     document.getElementsByClassName('container')[0].style.height = window.innerHeight-20 + 'px';
 }
 
-// function about game load
-
+/**
+ * function about game load
+ */
 pageLoaded();
 function pageLoaded(){
     // let plates =  document.getElementsByClassName('plate');
@@ -55,6 +61,10 @@ function pageLoaded(){
 
     initGlobalVar();
 }
+
+/**
+ * Initialize the global variable in the game to start a new turn
+ */
 function initGlobalVar(){
     g_IsGameStart = false;
     g_IsUnlockLighten = false;
@@ -73,20 +83,38 @@ function initGlobalVar(){
     };
 }
 
-// function about game
-
+/**
+ * Function about game
+ */
+/**
+ * Click event of Start game button is clicked
+ */
 function startClick(){
+    /**
+     * Start button can not be clicked after the is begun.
+     */
     if (g_IsGameStart)
         return false;
 
     g_IsGameStart = true;
+    /**
+     * Clear the class make sure it would stuck(this function just for fix some bug)
+     */
     clearLightenClass();
 
+    /**
+     * Turn on the light
+     */
     toggleBulb();
     let startButton = document.getElementById('start');
     startButton.innerHTML = __STATUS_TEXT__.playing;
     startButton.style.cursor = 'not-allowed';
 
+    document.getElementById('score-current').innerHTML = '00';
+
+    /**
+     * Game will start after __START_WAIT__ seconds later.
+     */
     setTimeout(winTime, __START_WAIT__);
     // addGameClickBox();
     // playGameBoxList(g_GameClickBox);
@@ -97,6 +125,9 @@ function startClick(){
     //     alert(g_GameClickBox[i]);
 }
 
+/**
+ * Execute the game, emptying players' operation, and start a delay timer.
+ */
 function winTime(){
     let levels = runGame(g_GameClickBox);
     g_PlayerClickBox = [];
@@ -105,6 +136,9 @@ function winTime(){
     }, g_DelayTimer.times + levels*g_Model_Current.duration);
 }
 
+/**
+ * Add a color into the game box for player memorizing it. and show all the color in the box that need player to memory.
+ */
 function runGame(gameBox){
     g_IsUnlockLighten = false;
     addGameClickBox();
@@ -115,6 +149,9 @@ function runGame(gameBox){
     // setTimeout(function(){}, g_GameClickBox);
 }
 
+/**
+ * Lighten the Plate one by one
+ */
 function playGameBoxList(gameBox){
     let items = document.getElementsByClassName('plate');
     let o = function (items, gameBox, i) {
@@ -140,6 +177,11 @@ function playGameBoxList(gameBox){
             // }
 }
 
+/**
+ * lighten a plate
+ * @param bulb - Which Plate needed to be lit
+ * @param currentModel - Which game speed should be used
+ */
 function lightenPlate(bulb, currentModel){
     // bulb.classList.remove('on');
     // if (bulb.classList.contains(currentModel.model))
@@ -159,6 +201,9 @@ function lightenPlate(bulb, currentModel){
     }, currentModel.duration, bulb, currentModel);
 }
 
+/**
+ * Add a color into the memory box.
+ */
 function addGameClickBox(){
     g_GameClickBox.push(__COLORS__[Math.floor(Math.random()*4)]);
 }
@@ -181,6 +226,9 @@ function addGameClickBox(){
 //     bulb.classList.remove('on');
 // }
 
+/**
+ * On-off light
+ */
 function toggleBulb(){
     let bulb = document.getElementsByClassName('light-box')[0];
     bulb.classList.toggle('on');
@@ -220,11 +268,20 @@ function plateClick(){
     // }
 }
 
+/**
+ * Player click the plate when game is running and the plate is not locked only.
+ */
 function plateDown(){
     if(!(g_IsGameStart && g_IsUnlockLighten))
         return false;
 
+    /**
+     * Show the plate player click
+     */
     showMousePlate(this);
+    /**
+     * Save the color which player click
+     */
     addPlayerClick(this.getAttribute('value'))
     clearTimeout(g_DelayTimer.timer);
     if(!checkGame())
@@ -249,23 +306,40 @@ function plateDown(){
 
 }
 
+/**
+ * Quench the plate after player click or mouseup at one plate.
+ */
 function plateUp(){
     //log: 更换before
     hiddenMousePlate(this);
 }
 
+/**
+ * Show the plate
+ */
 function showMousePlate(obj) {
     obj.style.opacity = '1';
 }
 
+/**
+ * hidden the plate
+ */
 function hiddenMousePlate(obj) {
     obj.style.opacity = '0.3';
 }
 
+
+/**
+ * Add the score into HTML Page after the round is over.
+ */
 function addScoreCurrent(){
     g_Score_Current++;
     setScoreCurrent(g_Score_Current);
 }
+
+/**
+ * Set the score into HTML Page after the round is over.
+ */
 function setScoreCurrent(score){
     if(score>9)
         document.getElementById('score-current').innerHTML = g_Score_Current;
@@ -273,6 +347,9 @@ function setScoreCurrent(score){
         document.getElementById('score-current').innerHTML = '0' + g_Score_Current;
 }
 
+/**
+ * Compare both of Current Score with The Top Score, and set the biggest on the HTML Page
+ */
 function setScoreTop(score) {
     if (score > 9)
         document.getElementById('score-top').innerHTML = g_Score_Top;
@@ -280,10 +357,16 @@ function setScoreTop(score) {
         document.getElementById('score-top').innerHTML = '0' + g_Score_Top;
 }
 
+/**
+ *  Add a color which player click
+ */
 function addPlayerClick(peek){
     g_PlayerClickBox.push(peek);
 }
 
+/**
+ * Compare the color that player clicked with saved in the game box that random by system
+ */
 function checkGame(){
     for (let i=0; i<g_PlayerClickBox.length; i++)
         if (g_GameClickBox[i] !== g_PlayerClickBox[i])
@@ -291,6 +374,9 @@ function checkGame(){
     return true;
 }
 
+/**
+ * Improve game speed
+ */
 function modelUp(){
     let keys = Object.keys(__PLATE_LIGHTEN_DURATION__);
     let i = keys.indexOf(g_Model_Current.model)
@@ -299,6 +385,9 @@ function modelUp(){
     g_Model_Current.duration = __PLATE_LIGHTEN_DURATION__[levelUp];
 }
 
+/**
+ * Set Score, lit all colors and initialize game variate
+ */
 function defeatTime(){
     g_Score_Top = g_Score_Current;
     setScoreTop(g_Score_Top);
@@ -306,6 +395,9 @@ function defeatTime(){
     gameOver();
 }
 
+/**
+ * Make all the lights flash
+ */
 function flashPlate(){
     clearLightenClass();
     let items = document.getElementsByClassName('plate');
@@ -326,6 +418,9 @@ function flashPlate(){
     // o(items, times, duration);
 }
 
+/**
+ * Clear the Class before it be used, this function just for fixing bug.
+ */
 function clearLightenClass() {
     let plates = document.getElementsByClassName('plate');
     let keys = Object.keys(__PLATE_LIGHTEN_DURATION__);
@@ -334,7 +429,9 @@ function clearLightenClass() {
             plates[i].classList.remove(keys[j])
 }
 
-
+/**
+ * Do something after game over.
+ */
 function gameOver(){
     initGlobalVar();
     toggleBulb();
@@ -344,12 +441,11 @@ function gameOver(){
 
 }
 
-function testt(){
-    document.getElementsByClassName('plate')[0].classList.remove('on-2000');
-}
-
-
 // NOTE Window
+/**
+ *  STATE: The following code just for show my Student Information,
+ *         Neither addition nor deletion will affect the functionality of Assignment-2
+ */
 /*The following JS is about my Student card only*/
 var titBar = document.getElementById("tit-bar");
 titBar.addEventListener("click", expandWindow);
